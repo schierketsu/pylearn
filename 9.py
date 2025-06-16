0bec51ec32c7656fbac1a83eb9d4bdcb11d6c0bd
schierketsu <crumplemi@gmail.com>
2025-06-16 21:10:39 +0300

    lyaaaa

---
+++ 9.py
@@ -0,0 +1,34 @@
+#УСЛОВНЫЕ ОПЕРАТОРЫ
+#1
+# age = int(input())
+# if age > 18:
+#     print('you >18') 
+# else:
+#     print('you little, wait ', 18-age) 
+
+# my_age = 21
+# youage = int(input())
+# if youage>my_age:
+#     print('you old')
+# elif youage<my_age:
+#     print('you young')
+# else:
+#     print('=')
+
+# a = int(input())
+# b = int(input())
+# if a>b:
+#     print(a, ">", b)
+# elif a<b:
+#     print(a, "<", b)    
+# else:
+#     print("=")
+
+# leto = ['june', 'jule', 'augst']
+# osen = ['sentybr', 'oktyabr', 'novemb']
+# zima = ['decabr', 'yanvar', 'fevr']
+# vesna = ['mart', 'apr', 'may']
+
+# month = input()
+# if month in leto:
+#     print('leto')
